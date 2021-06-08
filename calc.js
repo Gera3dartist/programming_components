d5125d953f395dea98d174056a4853997ef039d0
Andrii Gerasymchuk <agerasymchuk@healthjoy.com>
2021-06-08 23:51:27 +0300

    implemented api

--- calc.js
+++ calc.js
@@ -1,5 +1,21 @@
-function ataylor(/*in*/ count){
-    return 10;
+
+function ataylor(xValue, numberOfIteration=10){
+    return sinFromInternet(xValue, numberOfIteration);
+}
+
+function sinFromInternet(x, iterNum) {
+    var mxx = -x*x;
+    var sin = 1;
+    var n = 0;
+    var term = 1;
+    for (var i = 1; i <= 2*iterNum; i++) {
+        n = n + 2;
+        term = term * mxx / ( n*(n+1) );
+        sin = sin + term
+    }
+    sin = x*sin;
+    return sin;
+
 }
 
 module.exports.taylor = ataylor;
